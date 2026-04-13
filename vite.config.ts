bd90c3d6660f5947dc3651c9311756329e85db31
comercialtechnet <admcomercial.technet@gmail.com>
2026-04-13 14:31:27 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/comercialtechnet",
+  base: "/comercialtechnet/",
   server: {
     host: "::",
     port: 8080,
